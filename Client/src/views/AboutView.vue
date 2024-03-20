d266708c36beafbc23ad936909a4ad8966ada76c
CyrusASC20 <mikac1@newpaltz.edu>
2024-03-19 22:00:24 -0400

    Initial commit

---
+++ Client/src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>This is an login page</h1>
+    <div class="container">
+      <RouterLink to="/">Moshe</RouterLink>
+      <RouterLink to="/about">Kamala Harris</RouterLink>
+      <RouterLink to="/">John SMith</RouterLink>
+    </div>
+  </div>
+
+
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ Client/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h1>Please Login</h1>
+    </div>
+    <div class="container">
+      <h2>Logging in as Moshe should give you access to the Activity page as well as the admin pages (right now just users).</h2>
+    </div>
+    <div class="container">
+      <h3>Logging in as other should give you only the Activity page</h3>
+    </div>
+  </main>
+</template>
